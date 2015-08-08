cb2b3a257695deb0905a738038ecf902981eec54
Andrew Aday <andrewladay@gmail.com>
2015-08-08 14:31:00 -0700

    map

--- client/views/home.js
+++ client/views/home.js
@@ -1,5 +1,21 @@
 Template.home.helpers({
-  
+  CampusMapOptions: function() {
+  	if (GoogleMaps.loaded()) {
+  		return {
+  			center: new google.maps.LatLng(-37.8136, 144.9631),
+  			zoom: 8
+  		};
+  	}
+  }
+});
+
+Template.home.onCreated(function() {
+	GoogleMaps.ready("CampusMap", function(map) {
+		var marker = new google.maps.Marker({
+			position: map.options.center,
+			map: map.instance
+		});
+	});
 });
 
 Template.home.events({
