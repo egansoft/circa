711cb9274876da6c1d1cc7d69bd0a13c8dab0fe8
Andrew Aday <andrewladay@gmail.com>
2015-08-08 21:05:57 -0700

    facebook pics

--- client/views/home.js
+++ client/views/home.js
@@ -35,6 +35,7 @@ Template.home.onCreated(function() {
 		);
 
 		var events = {};
+		var infowindow;
 
 		Events.find().observe({
 			added: function(document) {
@@ -46,12 +47,46 @@ Template.home.onCreated(function() {
 					icon: Icons[document.category],
 					id: document._id
 				});
-				google.maps.event.addListener(marker, 'dragend', function(event) {
-					Events.update(marker.id, {$set : {lat: event.latLng.lat(), lng: event.latLng.lng()}});
-				});
-				google.maps.event.addListener(marker, 'rightclick',
+				// google.maps.event.addListener(marker, 'dragend', function(event) {
+				// 	Events.update(marker.id, {$set : {lat: event.latLng.lat(), lng: event.latLng.lng()}});
+				// });
+
+				google.maps.event.addListener(marker, 'mouseover', 
+					function(){
+						infowindow = new google.maps.InfoWindow({
+							content: '<div class="content">'+
+						      '<h1 class="eventHeading">' + document.name + '--' + document.category + '</h1>'+
+						      '<div id=' + marker.id + '>'+
+						      '<p class="event-time-info-window"><strong>' + document.startTime + ' - ' + document.endTime + '</strong></p>' +
+						      '<p><span class="event-capacity-info-window">'  + document.attending.length + '/' + document.capacity + '</span> people attending</p>' +
+						      '<p>' + document.description + '</p>'+
+						      '<ul class="attending"></ul>' +
+						      '<p id="rsvp-notice"></p>'+
+						      '</div>'+
+						      '</div>'
+						});
+						infowindow.open(GoogleMaps.maps.CampusMap.instance, marker);
+						if ($('#' + marker.id + ' .attending li').length < 1) {
+							document.attending.forEach(function(entry) {
+								$('#' + marker.id + ' .attending').append("<li><img src=http://graph.facebook.com/" + entry + "/picture/?type=small></li>");
+							});
+						}
+					}
+				);
+
+				google.maps.event.addListener(marker, 'mouseout',
+					function(){
+						infowindow.close(GoogleMaps.maps.CampusMap.instance, marker);
+					}
+				);
+
+				google.maps.event.addListener(marker, 'click',
 					function(event) {
-						Events.remove(marker.id);
+						if (document.host != Meteor.user()._id) {
+							$('#rsvp-notice').text("You're going!");
+							$('#' + marker.id + ' .attending').append("<li><img src=http://graph.facebook.com/" + Meteor.user().services.facebook.id + "/picture/?type=large></li>");
+							Events.update(marker.id, {$addToSet: {attending: Meteor.user().services.facebook.id}});
+						}
 					}
 				);
 
@@ -59,6 +94,14 @@ Template.home.onCreated(function() {
 			},
 			changed: function(newDocument, oldDocument) {
 				events[newDocument._id].setPosition({lat: newDocument.lat, lng: newDocument.lng});
+				var att = $('#' + newDocument._id +' .event-capacity-info-window').text();
+				console.log("HELLO");
+				console.log('the string is' + att);
+				var capacity = att.substring(att.indexOf('/')); 
+				console.log('cap is ' + capacity);
+				att2 = parseInt(att.substring(0, att.indexOf('/')), 10) + 1;
+				console.log('att2 is ' + att2);
+				$('#' + newDocument._id +' .event-capacity-info-window').text(att2.toString() + capacity);
 			},
 			removed: function(oldDocument) {
 				events[oldDocument._id].setMap(null);
@@ -100,6 +143,7 @@ Template.CreateEventModal.events({
 			endTime: eventEndTime,
 			capacity: eventCapacity,
 			category: eventCategory,
+			attending: [Meteor.user().services.facebook.id],
 			host: Meteor.user()._id
 		});
 		Modal.hide('CreateEventModal');
